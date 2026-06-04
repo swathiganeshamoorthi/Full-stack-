3236be77a8dbf145d551ab1f936c08122543ec88
sam naren <synnarengmail.com>
2026-06-04 15:39:50 +0530

    JavaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,91 @@
+var a=10;
+let b=5;
+const c=2;
+
+var ab;
+ab=19;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(ab);
+
+var num=34;
+var str="swathi";
+var underfine;
+var Null=null;
+var boolean=true;
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(underfine);
+console.log(typeof Null);
+console.log(typeof boolean);
+
+var bigInt=123n;
+var symbol=Symbol('li');
+
+console.log(typeof bigInt);
+console.log(typeof symbol);
+
+var arr=[ 10,20,30];
+console.log(typeof arr ,arr);
+var obj={
+    name:"swathi",
+    dept:"CT"
+
+}
+console.log(typeof obj,obj)
+
+
+//arthmetic operator(+,-,*,/,%)
+var s=10;
+var t="swathi";
+
+console.log(s+t);
+console.log(s-t);
+console.log(s*t);
+console.log(s/t);
+console.log(s%t);
+
+//logical operator
+var x=true;
+var y=false;
+
+console.log(x&&y);
+console.log(x||y);
+console.log(!x);
+
+//RElationl operator
+
+var e=10;
+var f=20;
+
+console.log(e<f);
+console.log(e>f);
+console.log(e<=f);
+console.log(e>=f);
+console.log(e==f);
+console.log(e===f);
+console.log(e!=f);
+console.log(e!==f);
+
+
+d=10;
+f=20;
+d+=f;//30
+console.log(d);
+
+d-=f;
+console.log(d);
+
+//uninary operator(--,++)
+
+var m=10;
+console.log(m++);
+console.log(++m);
+
+var n=20;
+console.log(n--);
+console.log(--n);
+
+
